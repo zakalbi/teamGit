35035beeef3c770b9c58b42c0c4bf36a3cff9939
Thomas <thomas.crowell@techie.com>
2021-02-09 16:59:25 -0500

    Final Submit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-// adding css to jsx is that easy
-import './App.css'; // This pattern is preferred where css for this component has a matching .css filename
-
-// A component import
-import Navbar from './components/Navbar'
-
-
-// Defining our <App /> component the function name matches the file name
+import { useEffect, useState } from 'react';
+import './App.css';
+import Nasa from './components/Nasa';
+import Navbar from './components/Navbar';
 function App() {
-  // All functional components need to return jsx with one parent element
-  return ( 
-    <div className="App"> {/* Parent Element. Also we can't use the word class, so we use className in jsx*/}
-      {/* Navbar is our imported component*/}
+  const [location, setLocation] = useState({ latitude: 0, longitude: 0 })
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+      setLocation(
+        {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+      );
+    })
+  }, [])
+  return (
+    <div className="App">
       <Navbar />
-      <h1>Develop</h1>
-      <h1>Tara</h1>
-      <h1>Tara 2</h1>
-      <h1> Thomas</h1>
-      <h1>Zak</h1>
+      {location.latitude}
+      {location.longitude}
+      <Nasa long={location.longitude} lat={location.latitude} />
     </div>
   );
 }
-
-// Makes our Component available for import
 export default App;
